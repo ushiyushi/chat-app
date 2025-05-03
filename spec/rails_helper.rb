2f0a8b1fd003e23307a1c72a9be7445037ae8f58
ushiyushi <ymurae46@gmail.com>
2025-05-03 17:41:41 +0900

    結合テストの実装

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -34,12 +34,15 @@ rescue ActiveRecord::PendingMigrationError => e
 end
 
 I18n.locale = "en"
+Rails.root.glob('spec/support/**/*.rb').sort_by(&:to_s).each { |f| require f }
 
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_paths = [
     Rails.root.join('spec/fixtures')
   ]
+
+  config.include SignInSupport
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
